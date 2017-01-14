0d513593edc0e80c75dd58f30ffdd8399321cd46
Tiago Martinho <tiago@elit.software>
2017-01-14 15:09:59 +0100

    Initial Commit

---
+++ SwiftFix/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SwiftFix
+//
+//  Created by Tiago Martinho on 14/01/2017.
+//  Copyright © 2017 Tiago Martinho. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
